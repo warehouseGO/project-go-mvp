a4a346af3b2081022839adf73eec4c25d17d1fc5
RAJAT DUGAR <rajat02514@gmail.com>
2025-08-10 23:34:24 +0530

    change

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 // const API_BASE_URL = "http://localhost:3000/api";
-
+console.log(import.meta.env.VITE_API_URL);
 // Create axios instance with base configuration
 const api = axios.create({
   baseURL: API_BASE_URL,
