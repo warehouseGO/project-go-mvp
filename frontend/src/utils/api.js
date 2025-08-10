9bac3464a0107e112e6007442865cc810f63fd99
RAJAT DUGAR <rajat02514@gmail.com>
2025-08-10 23:32:13 +0530

    login fixed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+// const API_BASE_URL = "http://localhost:3000/api";
 
 // Create axios instance with base configuration
 const api = axios.create({
